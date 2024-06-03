c7b32a9b0796d818e6fdecd24d9f81b0f7d0a924
qwezsd <avwds@naver.com>
2024-06-03 23:49:39 +0900

    gallery

--- src/wrap/main/Person1Component.jsx
+++ src/wrap/main/Person1Component.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import axios from 'axios'
 
-export default function Person1Component ({closeIntroduce}) {
+export default function Person1Component () {
 
-
+    const [state, setState] = React.useState({
+        person1: []
+    })
+    
+    axios({
+        url : './data/person1.json',
+        method : 'GET'
+    })
+    .then((res)=>{
+        setState({
+            person1 : res.data.person1
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 
     return (
         <div id='person1'>
             <div className="container">
                 <div className="content">
-
+                    <div className="img-box">
+                        <ul>
+                        {state.person1.map((item, inx)=>{
+                            return (
+                                <li key={item.번호}>
+                                    <img src={`./img/${item.이미지}`} alt="" />
+                                </li>
+                            )
+                        })
+                    }
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
