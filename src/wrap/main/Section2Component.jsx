c014fffa93ab67b34390b473bed8602b98d4fb18
qwezsd <avwds@naver.com>
2024-06-14 16:56:33 +0900

    gallery

--- src/wrap/main/Section2Component.jsx
+++ src/wrap/main/Section2Component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import './scss/section2.scss'
 
 export default function Section2Component () {
     const [state, setState] = React.useState({
